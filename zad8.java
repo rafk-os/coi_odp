af8ea2de534caec8650dfb424a486db5e88c5aae
rafk-os <61945072+rafk-os@users.noreply.github.com>
2021-12-07 14:34:32 +0100

    Update zad8.java

--- zad8.java
+++ zad8.java
@@ -1,4 +1,3 @@
- String[] drzewa = {"brzoza", "modrzew", "grab", "buk", "wierzba" };
-
-        Iterator<String> it = Arrays.stream(drzewa).iterator();
-        while(it.hasNext()) System.out.println(it.next());
+String[] drzewa = {"brzoza", "modrzew", "grab", "buk", "wierzba" };
+Iterator<String> it = Arrays.stream(drzewa).iterator();
+while(it.hasNext()) System.out.println(it.next());
